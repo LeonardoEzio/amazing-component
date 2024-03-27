24c9f6fa3a6821dc11c2ac23bf5750f97f07fdbb
LeonardoEzio <LeonardoEzio@163.com>
2024-03-28 00:55:32 +0800

    init

---
+++ xxljob-auto-register/src/main/java/leonardo/ezio/personal/component/XxlJobAutoRegister.java
@@ -0,0 +1,10 @@
+package leonardo.ezio.personal.component;
+
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class XxlJobAutoRegister implements BeanPostProcessor {
+
+
+}
